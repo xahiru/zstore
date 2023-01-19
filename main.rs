7a1a8fe9add9731de9112df0d5b1a2f1f831ea4c
Ahmed Zahir <xahiru@gmail.com>
2023-01-19 22:57:43 +0800

    imports added

--- main.rs
+++ main.rs
@@ -3,16 +3,16 @@ use std::io;
 use rand::Rng;
 use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 // Ordering compares values
 use std::cmp::Ordering;
 
-fn say_hello(){
-    println!("Hello");
-}
+//mods
+mod block;
+
 
 fn main () {
-    say_hello();
-    //   let difficulty = 0x000fffffffffffffffffffffffffffff;
-    //   let mut genesis_block = Block::new(0, now(), vec![0; 32],vec![Transaction {inputs: vec![ ],outputs: vec![ transaction::Output { to_addr: "Alice".to_owned(),value: 50,},transaction::Output {  to_addr: "Bob".to_owned(),value: 7,},],},], difficulty);
+    let difficulty = 0x000fffffffffffffffffffffffffffff;
+    // let mut genesis_block = block::Block::new(0, now(), vec![0; 32],vec![Transaction {inputs: vec![ ],outputs: vec![ transaction::Output { to_addr: "Alice".to_owned(),value: 50,},transaction::Output {  to_addr: "Bob".to_owned(),value: 7,},],},], difficulty);
 }
