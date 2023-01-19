342b7ad98ad419e817199fed50e216c870863b67
Ahmed Zahir <xahiru@gmail.com>
2023-01-19 22:13:30 +0800

    block struck n implementation

---
+++ block.rs
@@ -0,0 +1,15 @@
+pub struct Block {
+    pub index: u32,
+    pub timestamp: u128,
+    pub hash: Hash,
+    pub prev_block_hash: Hash,
+    pub nonce: u64,
+    pub transactions: Vec,
+    pub difficulty: u128,
+}
+
+impl Block{
+    pub fn new(index: u32, timestamp, u128, prev_block_hash, Hash, transactions: Vec, difficulty: u128)->{
+        Block{index, timestamp, hash:vec![0;32], prev_block_hash, nonce}
+    }
+}
